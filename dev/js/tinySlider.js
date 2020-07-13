eece628fb2c0c4d35975c5ae167114246e3509ee
adevine104 <63673128+adevine104@users.noreply.github.com>
2020-07-13 02:02:14 -0400

    footer and slider and spacing

--- dev/js/tinySlider.js
+++ dev/js/tinySlider.js
@@ -4,7 +4,7 @@ tns({
     container: '.my-slider',
     items: 1,
     gutter: 10,
-    edgePadding: 50,
+    // edgePadding: 50,
     controls: false,
     controlsPosition: "bottom",
     navPosition: "bottom",
@@ -12,17 +12,17 @@ tns({
     //loop: false,
     //startIndex: 2,
 
-    // responsive: {
-    //       "350": {
-    //         "items": 1
-    //       },
-    //       "768": {
-    //         "items": 2
-    //       },
-    //       "1440": {
-    //         "items": 3
-    //       }
-    // }
+    responsive: {
+          "350": {
+            "items": 1
+          },
+          "768": {
+            "items": 3
+          },
+          "1440": {
+            "items": 3
+          }
+    },
     
     //slideBy: 'page',
     autoplay: true,
