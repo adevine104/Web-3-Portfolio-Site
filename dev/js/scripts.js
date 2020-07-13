c0ee520bd30a41dc85ddb81ad7e271becb810087
adevine104 <63673128+adevine104@users.noreply.github.com>
2020-07-13 16:40:45 -0400

    footer

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,3 +6,26 @@ $( document ).ready(function() {
  });
  
  
+ var $burger = $("#burger-icon");
+ var $nav = $('nav[data-nav]');
+ var $navA = $('nav[data-nav] a');
+ var isVisible = false;
+  
+ function mouseClick(){
+  
+ if($(window).width() <= 992){
+ console.log("click");
+ if(isVisible === false){
+ $nav.show();
+ isVisible = true;
+ }
+ else{
+ $nav.hide();
+ isVisible = false;
+ }
+ }
+ }
+  
+ $burger.on( "click", mouseClick);
+ // close the mobile menu when menu link is clicked
+ $navA.on( "click", mouseClick);
