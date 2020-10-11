f23a8aa6f1cda7c6f648c7aa3405d3fb93db8cc9
adevine104 <63673128+adevine104@users.noreply.github.com>
2020-10-11 10:42:24 -0400

    header and accordion fixes

--- dev/js/tinySlider.js
+++ dev/js/tinySlider.js
@@ -36,3 +36,42 @@ if( $(".slides").hasClass("my-slider") ){
     autoplayPosition: "bottom"
 });
 }
+
+// console.log("tiny slider is working!");
+
+if( $(".slides2").hasClass("my-slider2") ){
+  tns({
+    container: '.my-slider2',
+    items: 3,
+    gutter: 10,
+    // edgePadding: 50,
+    controls: false,
+    controlsPosition: "bottom",
+    navPosition: "bottom",
+    arrowKeys: true,
+    //loop: false,
+    //startIndex: 2,
+
+    responsive: {
+          "350": {
+            "items": 1
+          },
+          "768": {
+            "items": 3
+          },
+          "1440": {
+            "items": 3
+          }
+    },
+    
+    //slideBy: 'page',
+    autoplay: true,
+    "autoplayText": [
+      "▶",
+      "❚❚"
+    ],
+    // autoplayButton: true,
+    // autoplayText: (),
+    autoplayPosition: "bottom"
+});
+}
