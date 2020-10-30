38ddc52e014ae96ffdc87ae14a2563f7a4cc2ead
adevine104 <63673128+adevine104@users.noreply.github.com>
2020-10-30 15:45:22 -0400

    BRANDING AND PRINT EDITS

--- dev/js/tinySlider.js
+++ dev/js/tinySlider.js
@@ -75,3 +75,42 @@ if( $(".slides2").hasClass("my-slider2") ){
     autoplayPosition: "bottom"
 });
 }
+
+
+
+if( $(".slides3").hasClass("my-slider3") ){
+  tns({
+    container: '.my-slider3',
+    items: 3,
+    gutter: 10,
+    // edgePadding: 50,
+    controls: false,
+    controlsPosition: "bottom",
+    navPosition: "bottom",
+    arrowKeys: true,
+    //loop: false,
+    //startIndex: 2,
+
+    responsive: {
+          "350": {
+            "items": 1
+          },
+          "768": {
+            "items": 3
+          },
+          "1440": {
+            "items": 3
+          }
+    },
+    
+    //slideBy: 'page',
+    autoplay: true,
+    "autoplayText": [
+      "▶",
+      "❚❚"
+    ],
+    // autoplayButton: true,
+    // autoplayText: (),
+    autoplayPosition: "bottom"
+});
+}
