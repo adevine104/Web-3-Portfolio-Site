e756dd2594e5bd4251940423caeaf919f9d36643
adevine104 <63673128+adevine104@users.noreply.github.com>
2020-10-30 23:26:51 -0400

    web updates

--- dev/js/tinySlider.js
+++ dev/js/tinySlider.js
@@ -114,3 +114,42 @@ if( $(".slides3").hasClass("my-slider3") ){
     autoplayPosition: "bottom"
 });
 }
+
+
+if( $(".slides4").hasClass("my-slider4") ){
+  tns({
+    container: '.my-slider4',
+    "edgePadding": 50,
+    items: 1,
+    gutter: 10,
+    // edgePadding: 50,
+    controls: false,
+    controlsPosition: "bottom",
+    navPosition: "bottom",
+    arrowKeys: true,
+    //loop: false,
+    //startIndex: 2,
+
+    responsive: {
+          "350": {
+            "items": 1
+          },
+          "768": {
+            "items": 3
+          },
+          "1440": {
+            "items": 3
+          }
+    },
+    
+    //slideBy: 'page',
+    autoplay: true,
+    "autoplayText": [
+      "▶",
+      "❚❚"
+    ],
+    // autoplayButton: true,
+    // autoplayText: (),
+    autoplayPosition: "bottom"
+});
+}
